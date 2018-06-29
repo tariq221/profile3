79c0b48567285dc3b897b37f64e02dcabc946d00
mraco <36960271+mraco@users.noreply.github.com>
2018-06-29 17:59:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -234,7 +234,7 @@ bot.on("message", async message => {
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
   let commandfile = bot.commands.get(cmd.slice(prefix.length));
-  if(commandfile) commandfile.run(bot,message,args);
+  if(commandfile) commandfile.run(bot,message,args, sql);
 
 
 });
